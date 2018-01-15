09da66048d74b1469271b4ee52e294e5f5357f57
Marko Nikic <darkblue.azurite@gmail.com>
2018-01-15 21:29:50 +0100

    implement SubmatrixSum solution

--- src/BonusExercises/SubmatrixSum.java
+++ src/BonusExercises/SubmatrixSum.java
@@ -8,13 +8,26 @@ public class SubmatrixSum {
 
     static int n;
     static int[][] M;
+    static int[][] auxM;
 
     static void preprocess() {
+        auxM = new int[n + 1][n + 1];
 
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                auxM[i][j] = auxM[i][j - 1] + M[i][j];
+            }
+        }
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                auxM[j][i] = auxM[j - 1][i] + auxM[j][i];
+            }
+        }
     }
 
     static int query(int a, int b, int c, int d) {
-        return 0;
+        return auxM[b][d] - auxM[b][c - 1] - auxM[a - 1][d] + auxM[a - 1][c - 1];
     }
 
     public static void read_and_solve(InputStream in, PrintStream out) {
