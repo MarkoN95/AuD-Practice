fca89e63e2f5a5b479dec0754ef325df037e45bc
Marko Nikic <darkblue.azurite@gmail.com>
2018-01-19 20:22:15 +0100

    implement dyno solution

--- src/BonusExercises/Dyno.java
+++ src/BonusExercises/Dyno.java
@@ -5,10 +5,44 @@ import java.io.PrintStream;
 import java.util.Scanner;
 
 public class Dyno {
-    public static int solve(final int L, final int D, final int C, final  int[] cacti) {
 
-        
-        return 0;
+    private static int indexOf(int[] arr, int elem) {
+        int lo = 0, hi = arr.length - 1;
+
+        while(lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if(arr[mid] > elem) {
+                hi = mid - 1;
+            }
+            else if(arr[mid] < elem) {
+                lo = mid + 1;
+            }
+            else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+
+    private static boolean has(int[] arr, int elem) {
+        return indexOf(arr, elem) != - 1;
+    }
+
+    public static int solve(final int L, final int D, final int C, final  int[] cacti) {
+        int[] DP = new int[L];
+
+        for(int i = 1; i < L; i++) {
+            if((DP[i - 1] == i - 1 || (i >= D && DP[i - D] == i - D)) && !has(cacti, i)) {
+                DP[i] = i;
+            }
+            else {
+                DP[i] = DP[i - 1];
+            }
+        }
+
+        return DP[L - 1];
     }
 
     public static void read_and_solve(InputStream in, PrintStream out) {
@@ -25,7 +59,7 @@ public class Dyno {
             for(int j=0; j<C; j++)
                 cacti[j] = scanner.nextInt();
 
-            System.out.println(solve(L, D, C, cacti));
+            out.println(solve(L, D, C, cacti));
         }
 
         scanner.close();
