bbc91e5d78846ef78a03717d5d354ecc8cdc1fa5
Marko Nikic <darkblue.azurite@gmail.com>
2018-01-31 20:06:50 +0100

    implement LightCoffee solution

--- src/BonusExercises/LightCoffee.java
+++ src/BonusExercises/LightCoffee.java
@@ -6,8 +6,39 @@ import java.util.Scanner;
 
 public class LightCoffee {
     public static int solve(int C, int n, int[] coin_values, int[] coin_weights) {
-        
-        return 0;
+        int[][] DP_even = new int[n + 1][C + 1];
+        int[][] DP_odd = new int[n + 1][C + 1];
+
+        for(int i = 1; i <= n; i++) {
+            int v_i = coin_values[i - 1];
+            int w_i = coin_weights[i - 1];
+
+            for(int j = 0; j <= C; j++) {
+                int even_max = DP_even[i - 1][j];
+                int odd_max = DP_odd[i - 1][j];
+
+                if(v_i == j) {
+                    odd_max = Math.max(odd_max, w_i);
+                }
+                else if(v_i < j) {
+                    int even_cp = DP_even[i - 1][j - v_i];
+                    int odd_cp = DP_odd[i - 1][j - v_i];
+
+                    if(even_cp > 0) {
+                        odd_max = Math.max(odd_max, even_cp + w_i);
+                    }
+
+                    if(odd_cp > 0) {
+                        even_max = Math.max(even_max, odd_cp + w_i);
+                    }
+                }
+
+                DP_even[i][j] = even_max;
+                DP_odd[i][j] = odd_max;
+            }
+        }
+
+        return DP_even[n][C];
     }
 
     public static void read_and_solve(InputStream in, PrintStream out) {
