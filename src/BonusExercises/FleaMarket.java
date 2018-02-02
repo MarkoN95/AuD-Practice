aaa16663015a53637f8bebd5341cae905059a4d8
Marko Nikic <darkblue.azurite@gmail.com>
2018-02-02 21:13:28 +0100

    add solution to FleaMarket

--- src/BonusExercises/FleaMarket.java
+++ src/BonusExercises/FleaMarket.java
@@ -8,8 +8,31 @@ public class FleaMarket {
 
     public static int solve(int n, int S, int W, int[] space, int[] weight, int[] price)
     {
+        int[][][] DP = new int[n + 1][S + 1][W + 1];
 
-        return 0;
+        for(int s = 1; s <= S; s++) {
+            for(int w = 0; w <= W; w++) {
+                DP[0][s][w] = Integer.MIN_VALUE;
+            }
+        }
+
+        for(int i = 1; i <= n; i++) {
+            for(int s = 0; s <= S; s++) {
+                for(int w = 0; w <= W; w++) {
+                    if(weight[i] <= w) {
+                        DP[i][s][w] = Math.max(
+                                DP[i - 1][s][w],
+                                DP[i - 1][Math.max(0, s - space[i])][w - weight[i]] + price[i]
+                        );
+                    }
+                    else {
+                        DP[i][s][w] = DP[i - 1][s][w];
+                    }
+                }
+            }
+        }
+
+        return DP[n][S][W];
     }
 
     public static void read_and_solve(InputStream in, PrintStream out) {
