adf7076c92ed5aece0fba081dcb7f87d5b380249
Marko Nikic <darkblue.azurite@gmail.com>
2018-02-05 21:00:09 +0100

    add extra test cases for CellTowers

--- test/PracticeExamsTest.java
+++ test/PracticeExamsTest.java
@@ -21,4 +21,49 @@ public class PracticeExamsTest extends AuDTest {
 
         assertEquals(expected, out.toString().replace("\r", ""));
     }
+
+    @Test
+    public void testCellTowersTest1() throws FileNotFoundException {
+        String inPath = "testCases/CellTowers/test1.input";
+        String outPath = "testCases/CellTowers/test1.output";
+
+        init(outPath);
+
+        CellTowers.read_and_solve(
+                new FileInputStream(inPath),
+                new PrintStream(out)
+        );
+
+        assertEquals(expected, out.toString().replace("\r", ""));
+    }
+
+    @Test
+    public void testCellTowersTest2() throws FileNotFoundException {
+        String inPath = "testCases/CellTowers/test2.input";
+        String outPath = "testCases/CellTowers/test2.output";
+
+        init(outPath);
+
+        CellTowers.read_and_solve(
+                new FileInputStream(inPath),
+                new PrintStream(out)
+        );
+
+        assertEquals(expected, out.toString().replace("\r", ""));
+    }
+
+    @Test
+    public void testCellTowersTest3() throws FileNotFoundException {
+        String inPath = "testCases/CellTowers/test3.input";
+        String outPath = "testCases/CellTowers/test3.output";
+
+        init(outPath);
+
+        CellTowers.read_and_solve(
+                new FileInputStream(inPath),
+                new PrintStream(out)
+        );
+
+        assertEquals(expected, out.toString().replace("\r", ""));
+    }
 }
